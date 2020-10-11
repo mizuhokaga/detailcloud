e8fd70acc79f07a922493b10f5d818a78fb6fdd0
mizuhokaga <mizuhokaga@gmail.com>
2020-10-11 16:14:58 +0800

    包含着springsecurity

---
+++ src/main/java/com/springcloud81_2/detailcloud/DetailcloudApplication.java
@@ -0,0 +1,12 @@
+package com.springcloud81_2.detailcloud;
+
+
+
+@SpringBootApplication
+public class DetailcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetailcloudApplication.class, args);
+    }
+
+}
